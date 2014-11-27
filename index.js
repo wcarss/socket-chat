ca5abaad916423c3933b5c05528b09484f0f7919
Wyatt Carss <carss.w@gmail.com>
2014-11-27 18:24:59 -0500

    Identify and recall sockets and senders.

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var moment = require('moment');
 
+var sockets = {};
+var sockets_by_name = {};
+
 console.logCopy = console.log.bind(console);
 
 console.log = function() {
@@ -17,18 +20,38 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-  socket.broadcast.emit("connect");
-  console.log("broadcasting 'connect' for socket "+socket.id);
+  var user = {
+    name: 'FakeName',
+    other_data: 'Garbage',
+    id: socket.id
+  };
+
+  console.log("emitting 'identify' for socket " + socket.id);
+  socket.emit('identify', user);
+  console.log("broadcasting 'connect' for socket " + socket.id);
+  //socket.broadcast.emit("connect", user);
+  sockets_by_name[user['name']] = socket.id;
+  sockets[socket.id] = user;
+
   socket.on('disconnect', function() {
-    console.log('"disconnect" received from client '+ socket.id +'; emitting "disconnect".');
-    io.emit("disconnect", "User disconnected.");
+    console.log('"disconnect" received from client ' + socket.id + '; emitting "disconnect".');
+    io.emit("disconnect", "User " + sockets[socket.id]['name'] + "disconnected.");
+    //delete sockets_by_name[sockets[socket.id]['name']];
+    //delete sockets[socket.id];
   });
+
   socket.on('chat message', function(msg) {
-    console.log('chat message "' + msg + '" received from socket '+socket.id+'; emitting chat message.');
-    io.emit('chat message', msg);
+    console.log('chat message "' + msg + '" received from socket ' + socket.id + '; emitting chat message.');
+    io.emit(
+      'chat message', {
+        message: msg,
+        user: sockets[socket.id]
+      }
+    );
   });
+
   socket.on('new user', function() {
-    console.log('"new user" received from socket '+socket.id+'; emitting "new user".');
+    console.log('"new user" received from socket ' + socket.id + '; emitting "new user".');
     io.emit('new user', 'User connected');
   });
 });
