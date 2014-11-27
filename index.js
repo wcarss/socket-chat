2b3e76f884c2972e082576a13575545ef0c90614
Wyatt Carss <carss.w@gmail.com>
2014-11-27 15:17:54 -0500

    Added console logging.

--- index.js
+++ index.js
@@ -8,13 +8,17 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   socket.broadcast.emit("connect");
+  console.log("broadcasting 'connect'");
   socket.on('disconnect', function() {
+    console.log('"disconnect" received; emitting "disconnect".');
     io.emit("disconnect", "User disconnected.");
   });
   socket.on('chat message', function(msg) {
+    console.log('chat message "' + msg + '" received; emitting chat message.');
     io.emit('chat message', msg);
   });
-  socket.on('new user', function(msg) {
+  socket.on('new user', function() {
+    console.log('"new user" received; emitting "new user".');
     io.emit('new user', 'User connected');
   });
 });
